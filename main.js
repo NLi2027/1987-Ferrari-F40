042b13514f27bfc844e74f662f783a64b8bdd59c
NLi2027 <gptabsolutemode@gmail.com>
2025-12-02 14:24:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById("navbar").innerHTML=`
     <nav>
-       <h3> <a href = "overview.html">Car</a> | <a href = "engine.html">Engine</a> </h3>
+       <h3> <a href = "index.html">Car</a> | <a href = "engine.html">Engine</a> </h3>
     </nav>
 
 
